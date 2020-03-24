b0ebb30428cb50b524aa5c1e0f4635c8d66196cd
Javier Homs Gimeno <javier.homs@gmail.com>
2020-03-25 00:01:38 +0100

    findAll completo

--- src/api/libro/libro.controller.ts
+++ src/api/libro/libro.controller.ts
@@ -2,8 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 import { Libro } from '../libro';
 @Controller('libro')
 export class LibroController {
-	@Get()
+	libros: Libro[] = [{'id':1, 'titulo':'Don Quijote de la Mancha','autor':'Cervantes','fecha':new Date(1605,1,1,0,0,0,0)},
+						{'id':2, 'titulo':'La Colmena','autor':'Camilo José Cela','fecha':new Date(1951,1,1,0,0,0,0)}];
+	@Get()					
 	findAll():Libro[] {
-		return [];
+		
+		return this.libros;
 	}
 }
