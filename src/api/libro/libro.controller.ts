053472ba9ed6e2dc1ef153a57ad9f1101320fa82
Javier Homs Gimeno <javier.homs@gmail.com>
2020-03-25 00:59:41 +0100

    GETs completo

--- src/api/libro/libro.controller.ts
+++ src/api/libro/libro.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Param,  Get } from '@nestjs/common';
 import { Libro } from '../libro';
 @Controller('libro')
 export class LibroController {
-	libros: Libro[] = [{'id':1, 'titulo':'Don Quijote de la Mancha','autor':'Cervantes','fecha':new Date(1605,1,1,0,0,0,0)},
+	libros: Libro[] = [{'id':1, 'titulo':'Don Quijote de la Mancha','autor':'Cervantes','fecha':new Date(1605,1,1,0,0,0,0)}, 
 						{'id':2, 'titulo':'La Colmena','autor':'Camilo José Cela','fecha':new Date(1951,1,1,0,0,0,0)}];
 	@Get()					
 	findAll():Libro[] {
 		
 		return this.libros;
 	}
+	
+	
+	@Get('/:id')
+	getById(@Param() params): Libro {
+		
+		let found: boolean = false;
+		let id: number = undefined;
+		
+		for(let i = 0; i < this.libros.length && !found; i++){
+			if (params.id == this.libros[i].id){
+				found = true;
+				id = i;
+			}
+		}
+
+		if (found){
+			return this.libros[id];
+		}else{
+			return null;
+		}
+		
+	}
+	
+	
 }
